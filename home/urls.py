661296e94c97e615feae4920f219f257efdf494f
Rayleigh <rayleigh190@gmail.com>
2022-01-30 00:49:16 +0900

    feat: add breakfast list filter

--- home/urls.py
+++ home/urls.py
@@ -7,5 +7,6 @@ app_name = 'home'
 urlpatterns = [
     path('', views.index, name='index'),
     path('breakfast-detail/', views.breakfast_detail, name='detail'),
-    path('breakfast-create/', views.breakfast_create, name='breakfast_create')
+    path('breakfast-create/', views.breakfast_create, name='breakfast_create'),
+    path('breakfast-detail/delete/<int:breakfast_id>', views.breakfast_delete, name='breakfast_delete')
 ]
--- home/views.py
+++ home/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Breakfast
 from django.utils import timezone
+import datetime
 
 
 def index(request):
@@ -16,7 +17,16 @@ def breakfast_detail(request):
     """
     Breakfast 신청자 출력
     """
-    breakfast_list = Breakfast.objects.order_by('-create_date')
+    x = datetime.datetime.now()
+    y = datetime.datetime(x.year, x.month, x.day, 8, 30, 0)
+    if x < y:
+        today = datetime.date.today()
+        today = today - datetime.timedelta(1)
+    else:
+        today = datetime.date.today()
+
+    start_time = datetime.datetime(today.year, today.month, today.day, 8, 30, 0)
+    breakfast_list = Breakfast.objects.filter(create_date__range=[start_time, timezone.now()]).order_by("-create_date")
     context = {'breakfast_list': breakfast_list}
     return render(request, 'home/breakfast_detail.html', context)
 
@@ -29,4 +39,9 @@ def breakfast_create(request):
     breakfast.save()
     return redirect('home:index')
 
+
+def breakfast_delete(request, breakfast_id):
+    breakfast = get_object_or_404(Breakfast, pk=breakfast_id)
+    breakfast.delete()
+    return redirect('home:detail')
 # Create your views here.
